fff8aa024e838301477020f2b6ca9c865584f074
ptaszekk <ptaszekjakub@gmail.com>
2023-11-14 12:09:28 +0100

    feat!: table component created

---
+++ src/app/components/users-table/users-table.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { User } from '@models/models';
+
+@Component({
+    selector: 'app-users-table',
+    standalone: true,
+    templateUrl: './users-table.component.html',
+    styleUrls: ['./users-table.component.scss'],
+    imports: [
+        NgForOf,
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UsersTableComponent {
+    @Input() users!: Array<User>;
+}
