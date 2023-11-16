5cc0471aedfe8da3f7dea37750fccae621fc137f
ptaszekk <ptaszekjakub@gmail.com>
2023-11-16 12:06:39 +0100

    fix: interfaces structure

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -9,21 +9,25 @@ export interface FetchedUser {
     'name': string;
     'username': string;
     'email': string;
-    'address': {
-        'street': string;
-        'suite': string;
-        'city': string;
-        'zipcode': string;
-        'geo': {
-            'lat': string;
-            'lng': string;
-        };
-    };
+    'address': Address;
     'phone': string;
     'website': string;
-    'company': {
-        'name': string;
-        'catchPhrase': string;
-        'bs': string;
+    'company': Company;
+}
+
+export interface Company {
+    'name': string;
+    'catchPhrase': string;
+    'bs': string;
+}
+
+export interface Address {
+    'street': string;
+    'suite': string;
+    'city': string;
+    'zipcode': string;
+    'geo': {
+        'lat': string;
+        'lng': string;
     };
 }
